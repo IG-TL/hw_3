c23b3d704ec9721bab735c7d94a9bd89f5df40d0
Igor Gordienko <gordienkoie94@gmail.com>
2024-03-19 16:12:02 +0300

    'print' deleted

--- test_fixtures.py
+++ test_fixtures.py
@@ -5,11 +5,8 @@ def test_google_positive():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
     browser.element('[id="search"]').should(have.text('QA.GURU — Курсы тестировщиков - онлайн-обучение ...'))
-    print('positive')
-
 
 def test_google_negative():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('фъъъъъъъяяяяяъъъъ').press_enter()
     browser.element('[id="botstuff"]').should(have.text('По запросу фъъъъъъъяяяяяъъъъ ничего не найдено. '))
-    print('negative')
